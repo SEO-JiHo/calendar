42118709f0d1d59981ecff92d894308eb19b02ab
SEO-JiHo <jhseo9601@gmail.com>
2022-02-21 21:36:56 +0900

    initial commit

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Calendar {
     public static void main(String[] args) {
         System.out.println("Hello, Calendar");
-    }
+
+        //입력한 달의 최대 일수 출력하기
+        System.out.println("몇 월?");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        int[] k = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        System.out.printf("%d월은 %d일 까지 있습니다.", a, k[a]);
+        }
+
 }
